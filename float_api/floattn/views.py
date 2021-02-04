fe24054dcddd84854095a408ec03578ffab124f0
Brian York <briandavidyork@gmail.com>
2021-02-03 20:27:46 -0500

    fake user view;

--- float_api/floattn/views.py
+++ float_api/floattn/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the float index.")
+
+
+def users(request):
+    data = {
+        "user": "fake user data"
+    }
+    return JsonResponse(data)
